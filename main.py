3cec8fb5af439c8d78fea0be7c21ba5692451e9d
baykan baycora <karacaybaykan@gmail.com>
2025-08-07 11:21:01 +0300

    get_persistent_session_id fonksiyonu eklendi

--- main.py
+++ main.py
@@ -1248,4 +1248,4 @@ Eğer kullanıcı sana gerçek bir tarihi olayla ilgisi olmayan bir hikâye, kon
                     )
                     st.session_state.conversation_summary = summary
                     st.rerun()
-
+#
